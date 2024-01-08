19a8e2037216f91d4c328d4477f4b444a70ee66f
m4ks1k <m4ks1k@gmail.com>
2024-01-08 22:44:52 +0300

    added shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
@@ -8,7 +9,9 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -21,5 +24,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
